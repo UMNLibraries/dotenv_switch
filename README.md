9747ae5545b6f4ac7fab26c39d13ca95030b74a2
Michael Berkowski <mjb@umn.edu>
2019-10-01 12:34:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Python Dotenv Switch
+Enables switching between sourced `.env` files based on the environment defined 
+in `$APP_ENV`.
---
+++ dotenv_switch/__init__.py
@@ -0,0 +1,9 @@
+import sys
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
+
+# Read $APP_ENV, default to 'test' for filename like .env.test
+app_env_file = '.env.' + (getenv('APP_ENV') or 'test').lower()
+
+# Load the requested dotenv file
+load_dotenv(find_dotenv(filename=app_env_file, raise_error_if_not_found=True))
