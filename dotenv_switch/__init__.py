89a57a2279465e422f4d5be283c3a1713f7f02dd
Michael Berkowski <mjb@umn.edu>
2019-10-02 15:40:39 -0500

    Default env is variable is overrideable rather than hard coded "test"

--- dotenv_switch/__init__.py
+++ dotenv_switch/__init__.py
@@ -4,13 +4,13 @@ from dotenv import load_dotenv, find_dotenv
 from dotenv_switch.exceptions import DotenvSwitchFileNotFoundError
 
 # Read $APP_ENV, default to 'test' for filename like .env.test
-def env_file(var='APP_ENV'):
-    return '.env.' + (getenv(var) or 'test').lower()
+def env_file(var='APP_ENV', default_env='test'):
+    return '.env.' + (getenv(var) or default_env).lower()
 
 # Load the requested dotenv file
 # Throw an exception if required but not found
-def load(var='APP_ENV', required=False):
+def load(var='APP_ENV', default_env='test', required=False):
     try:
-        load_dotenv(find_dotenv(filename=env_file(var), raise_error_if_not_found=required, usecwd=True))
+        load_dotenv(find_dotenv(filename=env_file(var, default_env), raise_error_if_not_found=required, usecwd=True))
     except IOError:
         raise DotenvSwitchFileNotFoundError(env_file())
