bfe58e42ea13596c0a5232c005dbc65c30cb633e
David Naughton <naughton@pop-os.localdomain>
2019-10-31 10:30:30 -0500

    Improves tests for cases with no APP_ENV var.

--- tests/test_auto_no_app_env.py
+++ tests/test_auto_no_app_env.py
@@ -1,18 +1,58 @@
 import os
 import pathlib
 
-def test_auto_no_app_env(create_dotenv_files, delete_dotenv_files):
-    if 'APP_ENV' in os.environ:
-        os.environ.pop('APP_ENV')
+test_var = 'TEST_VAR'
+for env_var in ('APP_ENV', test_var):
+    if env_var in os.environ:
+        os.environ.pop(env_var)
 
-    test_var = 'TEST_VAR'
+def test_no_dotenv_files():
+    import dotenv_switch.auto
+    assert os.getenv(test_var) == None
+
+def test_dotenv_only(create_dotenv_files, delete_dotenv_files):
+    if test_var in os.environ:
+        os.environ.pop(test_var)
+    dotenv_file_metadata = {
+        '.env': {test_var: 'dotenv'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    import dotenv_switch.auto
+    assert os.getenv(test_var) == dotenv_file_metadata['.env'][test_var]
+    delete_dotenv_files(dotenv_files)
+
+def test_all_fallbacks(create_dotenv_files, delete_dotenv_files):
+    if test_var in os.environ:
+        os.environ.pop(test_var)
     dotenv_file_metadata = {
         '.env.test': {test_var: 'dotenv test'},
         '.env': {test_var: 'dotenv'},
     }
     dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
-
     import dotenv_switch.auto
     assert os.getenv(test_var) == dotenv_file_metadata['.env.test'][test_var]
+    delete_dotenv_files(dotenv_files)
 
+def test_all_fallbacks_and_dotenv_foo(create_dotenv_files, delete_dotenv_files):
+    if test_var in os.environ:
+        os.environ.pop(test_var)
+    dotenv_file_metadata = {
+        '.env.foo': {test_var: 'dotenv foo'},
+        '.env.test': {test_var: 'dotenv test'},
+        '.env': {test_var: 'dotenv'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    import dotenv_switch.auto
+    assert os.getenv(test_var) == dotenv_file_metadata['.env.test'][test_var]
+    delete_dotenv_files(dotenv_files)
+
+def test_dotenv_foo_only(create_dotenv_files, delete_dotenv_files):
+    if test_var in os.environ:
+        os.environ.pop(test_var)
+    dotenv_file_metadata = {
+        '.env.foo': {test_var: 'dotenv foo'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    import dotenv_switch.auto
+    assert os.getenv(test_var) == None
     delete_dotenv_files(dotenv_files)
--- tests/test_defaults_no_app_env.py
+++ tests/test_defaults_no_app_env.py
@@ -29,3 +29,23 @@ def test_all_fallbacks(create_dotenv_files, delete_dotenv_files):
     ds.load(override=True)
     assert os.getenv(test_var) == dotenv_file_metadata['.env.test'][test_var]
     delete_dotenv_files(dotenv_files)
+
+def test_all_fallbacks_and_dotenv_foo(create_dotenv_files, delete_dotenv_files):
+    dotenv_file_metadata = {
+        '.env.foo': {test_var: 'dotenv foo'},
+        '.env.test': {test_var: 'dotenv test'},
+        '.env': {test_var: 'dotenv'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    ds.load(override=True)
+    assert os.getenv(test_var) == dotenv_file_metadata['.env.test'][test_var]
+    delete_dotenv_files(dotenv_files)
+
+def test_dotenv_foo_only(create_dotenv_files, delete_dotenv_files):
+    dotenv_file_metadata = {
+        '.env.foo': {test_var: 'dotenv foo'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    ds.load(override=True)
+    assert os.getenv(test_var) == None
+    delete_dotenv_files(dotenv_files)
