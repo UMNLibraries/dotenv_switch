13568b9eb9e5585e3306a0c27b9c86c2bfc33baf
David Naughton <naughton@pop-os.localdomain>
2019-10-31 14:00:05 -0500

    Adds custom fallbacks tests where fallbacks = [].

--- tests/test_custom_fallbacks.py
+++ tests/test_custom_fallbacks.py
@@ -47,6 +47,29 @@ def test_all_fallbacks_and_dotenv_foo(create_dotenv_files, delete_dotenv_files):
     assert os.getenv(test_var) == dotenv_file_metadata['.env.foo'][test_var]
     delete_dotenv_files(dotenv_files)
 
+def test_no_fallbacks(create_dotenv_files, delete_dotenv_files):
+    dotenv_file_metadata = {
+        '.env.dev': {test_var: 'dotenv dev'},
+        '.env.stg': {test_var: 'dotenv stg'},
+        '.env.prd': {test_var: 'dotenv prd'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    ds.load(fallbacks=[], override=True)
+    assert os.getenv(test_var) is None
+    delete_dotenv_files(dotenv_files)
+
+def test_no_fallbacks_and_dotenv_foo(create_dotenv_files, delete_dotenv_files):
+    dotenv_file_metadata = {
+        '.env.foo': {test_var: 'dotenv foo'},
+        '.env.dev': {test_var: 'dotenv dev'},
+        '.env.stg': {test_var: 'dotenv stg'},
+        '.env.prd': {test_var: 'dotenv prd'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    ds.load(fallbacks=[], override=True)
+    assert os.getenv(test_var) == dotenv_file_metadata['.env.foo'][test_var]
+    delete_dotenv_files(dotenv_files)
+
 def test_dotenv_foo_only(create_dotenv_files, delete_dotenv_files):
     dotenv_file_metadata = {
         '.env.foo': {test_var: 'dotenv foo'},
