07bd8ecff8950f6064e8da43355b179700e0dab1
David Naughton <naughton@pop-os.localdomain>
2019-10-31 14:03:26 -0500

    Adds custom var tests where var is None.

--- tests/test_custom_var.py
+++ tests/test_custom_var.py
@@ -18,3 +18,12 @@ def test_custom_var(create_dotenv_files, delete_dotenv_files):
     assert os.getenv(test_var) == dotenv_file_metadata['.env.foo'][test_var]
     delete_dotenv_files(dotenv_files)
 
+def test_custom_var_is_none(create_dotenv_files, delete_dotenv_files):
+    dotenv_file_metadata = {
+        '.env.test': {test_var: 'dotenv test'},
+    }
+    dotenv_files = create_dotenv_files(pathlib.Path.cwd(), dotenv_file_metadata)
+    ds.load(var=None)
+    assert os.getenv(test_var) == dotenv_file_metadata['.env.test'][test_var]
+    delete_dotenv_files(dotenv_files)
+
