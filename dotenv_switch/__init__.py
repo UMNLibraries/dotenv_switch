1721906e1cfe3781f7fc5c9f4a6cba7b3610fbc8
David Naughton <naughton@pop-os.localdomain>
2019-11-01 17:07:45 -0500

    Adds most of a docstring for dotenv_switch.load().

--- dotenv_switch/__init__.py
+++ dotenv_switch/__init__.py
@@ -8,9 +8,39 @@ default_fallbacks = ['.env.test', '.env']
 default_required = False
 default_usecwd = False
 
-# Load the requested dotenv file
-# Throw an exception if required but not found
 def load(var=default_var, fallbacks=default_fallbacks, required=default_required, usecwd=default_usecwd, **kwargs):
+    '''Loads a dotenv file based on an env var and a list of fallback filenames
+
+    Parameters
+    ----------
+    var : str, optional
+        name of the env var specifying the desired environemnt
+        (default is APP_ENV)
+    fallbacks : list, optional
+        list of dotenv filenames to use if var is None or .env.{var} is
+        not found (default is ['.env.test','.env'])
+    required : bool, optional
+        whether to raise an exception if no dotenv file is found
+        (default is False)
+    usecwd : bool, optional
+        dotenv.find_dotenv() param, with the same default, which will be
+        passed to that function (default is False)
+    **kwargs
+        any remaining params will be passed to dotenv.load_dotenv()
+        
+    Raises
+    ------
+    DotenvSwitchUnspecifiedFilesRequiredError
+        If required is True, but var is None or .env.{var} cannot be
+        found, and fallbacks is None or empty.
+    DotenvSwitchFileNotFoundError
+        If required is True but no dotenv file can be found.
+
+    Returns
+    -------
+    DotEnv
+       the object returned by dotenv.loadenv()
+    '''
 
     filenames = fallbacks.copy() if isinstance(fallbacks, list) else []
     if isinstance(var, str) and getenv(var):
@@ -36,5 +66,4 @@ def load(var=default_var, fallbacks=default_fallbacks, required=default_required
 
     if required:
         raise DotenvSwitchFileNotFoundError(filenames_not_found)
-    # effectively return an empty dotenv for consistency
     return load_dotenv(**kwargs)
